9a2ed789c79acce664ec0088d69625ef576efcfc
destradax <destradax@gmail.com>
2018-10-28 16:57:04 -0500

    Added react-router-dom and basic landing page.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
 import './App.scss';
+import Clients from './clients';
+import Home from './home';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/clients">Clients</Link>
+            </li>
+          </ul>
+
+          <hr />
+
+          <Route exact path="/" component={Home} />
+          <Route path="/clients" component={Clients} />
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/clients.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class Clients extends React.Component {
+  render() {
+    return (
+      <div className="clients">Clients</div>
+    );
+  }
+}
